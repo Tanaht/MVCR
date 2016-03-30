f9b12178d65304b52013c538dbe7cdb24fb09cd9
Tanaky <charp.antoine@gmail.com>
2016-03-30 09:25:31 +0200

    Update ToListFilter.php
    Création de la logique (a améliorer avec du contrôle d'erreur

--- app/util/ToListFilter.php
+++ app/util/ToListFilter.php
@@ -4,5 +4,26 @@
 namespace app/util;
 
 class ToListFilter extends Filter {
-
+  public function __construct() {
+		parent::__construct("list");
+	}
+	
+	public function filter($before, array $args = null, $globals){
+	  $listValue = $args[0];
+	  $list = "<ul>";
+	  $listContent = "";
+	  
+	  foreach($before as $listItem) {
+	    if(is_object($listItem)) {
+	      $listContent .= "<li>" . $listItem->$listValue . "</li>";
+	    }
+	    
+	    if(is_array($listItem)) {
+	       $listContent .= "<li>" . $listItem[$listValue] . "</li>";
+	    }
+	  }
+	  $list .= $listContent;
+    $list .= "</ul>";
+		return $before . " €";
+	}
 }
